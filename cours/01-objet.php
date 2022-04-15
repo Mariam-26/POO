c04d2425b8bd57db4b056b559e45405792d7cf19
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-04-15 21:34:37 +0200

    POO

---
+++ cours/01-objet.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>L'objet -cous programmation orentée objet</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="p-5 bg-light">
+        <div class="container">
+            <h1 class="display-3">POO :La programmation orientée objet </h1>
+            <p class="lead"></p>
+
+            <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+            </p>
+        </div>
+    </div>
+
+
+
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-12">
+
+                <h2 class="text-center"> Qu'est ce que sont les oblets </h2>
+
+                <p>Les objets ressemblent fortement aux variables que l'on connait (string, integer, float, array, etc) mais ces dernières sont limitées et on peut se retrouver bloqué. Par exemple lorsque l'on crée un réseau social et que l'on veut sauvegarder un utilisateur.A ce moment là on voudra définier ses limites, ses droits, etc. Ces tableaux deviennent vite très complexes et si on doit transmettre note code, les personnes qui ne l'on pas écrit seront perdues .Un autre exemple ou le procédural est compliqué à appliquer est lorsque l'on s'occupe des dates.Elles sont en effet considérées comme des chaines de catacyères et faire des ajouts de jours ou de mois dessus devient très compliqué. Il faudra convertir les données et passer d'un format à l'autre .En orienté objet, les manipulations de date seront plus simples.</p>
+
+                <p class="alert alert-info">
+
+                    <strong>MaDate</strong> est une classe <br> <strong>$date1 = new MaDate;</strong>sont des objets ou bien des instanciations de cette classe.
+
+                </p>
+
+                <div class="col-12">
+
+                    <h2 class="text-center">Les propriètés</h2>
+
+
+                    <p>Sur nos différents objets (ou instanciations) nous allons avoir des propriétés, des variables.Un objet pourra contenir plusieurs propriéts mais aussi plusiurs méthodes: sur une classe maDate on pourra imaginer des propriétés pour afficher le nombre de jours mais aussi des méthodes pour ajouter des jours ou des mois .</p>
+
+                </div>
+
+                <div class="col-12">
+
+<h2 class="text-center">Les méthodes</h2>
+
+<p>Comme nous en avons parlé au dessus, les classes peuvent aussi contenir des méthodes. Ces dernières sont en fait des fonctions, appelées méthode dans le cadre de la POO. Elles ont donc la même nomenclature qu'en procédural : <code>nomMethode();</code></p>
+                </div>
+
+
+
+            </div>
+        </div>
+
+
+    </div>
+
+
+
+
+
+
+
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+
+</html>
