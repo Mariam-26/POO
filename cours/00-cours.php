7ba55b3198a02084fdc36edb917fe695b3f66d7a
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-04-15 21:34:24 +0200

    POO

---
+++ cours/00-cours.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Intro -Cours programmation orientée objet</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  </head>
+  <body>
+      
+<div class="p-5 bg-light">
+    <div class="container">
+        <h1 class="display-3">POO :La programmation orientée objet    </h1>
+        <p class="lead">Introduction</p>
+       
+            <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+        </p>
+    </div>
+</div><!-- fin du jumbotron -->
+
+
+
+<div class="container">
+
+<div class="row">
+
+
+<div class="col-12">
+<h2 class="text-center"> Historique</h2>
+
+<p>La notion d'objet a été introduite dans les langages de programmation en 1962 par le langage simlula.Cette façon de programmer a évoluée avant d'être popularisée en 1983 avec la sortie du langage C++, un langage orienté objet dont l'utilisation ressemble à celle du langage C.</p>
+<p>Cette nouvelle façon de programmer répond à plusieurs besoins.</p>
+<ul>
+
+<li> Elle facilite les changements importants dans le programme</li>
+<li>En cas de changements dans les langages , elle facilite la mise à niveau.</li>
+</ul>
+
+<p>La POO va permettre de faire façce aux changements majeurs qui auraient pu demander de reprendre un programme de A à Z. Mais elle va demander également une nouvelle façon de programmer et de réfléchir à l'organisation du code. 3 stratégies sont alors à mettre en place</p>
+
+<ul>
+
+<li>Modéliser différemment</li>
+<li>Modulariser</li>
+<li>Encapsuler</li>
+</ul>
+</div><!-- fin de la col-12 -->
+
+
+
+<div class="col-12">
+
+<h2 class="text-center">Les avantages de la POO </h2>
+
+
+<P>Le principal avantage de POO est de faciliter la réutilisation d'une module.Ainsi, on pourra reprendre les méthodes (fonctions) d'une classe existante dans une nouvelle classe grâce à la notion d'géritage.  </P>
+<p>Une autre chose facilitée par l'orienté objet est la lecture du code.En effet on comprendra plus facilement une instruction <code>->ajout</code>, qu'un if elese avec des multiples conditions dans une boucle. La force de cette façon de programmer est qu'elle se calque sur la réalité physique.Une classe sera par exemple Renault.Les objets, ou instanciations de cette classe seront $voiture1, $voiture2, etc.L'objet donc la voiture. Cet objet aura des caractéristiques représentées  par des variables, appelées propriétés, et des comportements représentées par des fonctions, appelées méthodes. En conclusion, les avantages de cette façon de programmer sont:</p>
+
+
+
+<ul>   
+    <li>La facilitation du travail collobaratif</li>
+
+    <li>La simplification de la maintenance</li>
+
+    <li>L'assouplissement du code</li>
+
+</ul>
+
+<p>Cette approche est toute de même moins intuitive que l'approche procédurale. La POO oblige en effet à réfléchir et modéliser avant de programmer</p>
+
+</div><!-- fin de la col-12 -->
+
+
+
+
+
+
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  </body>
+</html>
